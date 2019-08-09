7ab46d017ea3d7ca7b99d8992d61308616436831
Guard Rover <guard.rover@gmail.com>
2019-08-09 18:39:46 +0300

    all methods added

--- src/ldb.cpp
+++ src/ldb.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include "leveldb/db.h"
 
 using namespace std;
@@ -18,5 +19,38 @@ int main()
         return -1;
     }
     cout << "OK" << endl;
+
+    // Add 256 values to the database
+    leveldb::WriteOptions writeOptions;
+    for (unsigned int i = 0; i < 256; ++i)
+    {
+        stringstream keyStream;
+        keyStream << "Key" << i;
+
+        stringstream valueStream;
+        valueStream << "Test data value: " << i;
+
+        db->Put(writeOptions, keyStream.str(), valueStream.str());
+    }
+
+    // Iterate over each item in the database and print them
+    leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
+
+    for (it->SeekToFirst(); it->Valid(); it->Next())
+    {
+        cout << it->key().ToString() << " : " << it->value().ToString() << endl;
+    }
+
+    if (false == it->status().ok())
+    {
+        cerr << "An error was found during the scan" << endl;
+        cerr << it->status().ToString() << endl;
+    }
+
+    delete it;
+
+    // Close the database
+    delete db;
+
     return 0;
 }
