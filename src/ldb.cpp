f5147f3801b07d732478d69e97a22687a6f02f04
Guard Rover <guard.rover@gmail.com>
2019-08-09 15:58:34 +0300

    src/ldb.cpp

---
+++ src/ldb.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "leveldb/db.h"
+
+using namespace std;
+
+int main()
+{
+    leveldb::DB* db;
+    leveldb::Options options;
+    options.create_if_missing = true;
+
+    leveldb::Status status = leveldb::DB::Open(options, "C:/Qt/Projects/temp/testdb", &db);
+
+    if (false == status.ok())
+    {
+        cerr << "Unable to open/create test database 'C:/Qt/Projects/temp/testdb'" << endl;
+        cerr << status.ToString() << endl;
+        return -1;
+    }
+    return 0;
+}
